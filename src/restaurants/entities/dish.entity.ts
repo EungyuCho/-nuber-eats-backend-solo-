270c7f2ec230390f899558cceeb4643faef1524f
gyu <cho2304@naver.com>
2021-01-21 15:28:45 +0900

    :bookmark: 11.3.0 : :sparkles: Edit Dish, Delete Dish

--- src/restaurants/entities/dish.entity.ts
+++ src/restaurants/entities/dish.entity.ts
@@ -4,14 +4,24 @@ import { CoreEntity } from '../../common/entities/core.entity';
 import { IsNumber, IsString, Length } from 'class-validator';
 import { Restaurant } from './restaurant.entity';
 
+@InputType('DishChoiceInputType', { isAbstract: true })
+@ObjectType()
+class DishChoice {
+  @Field((type) => String)
+  name: string;
+
+  @Field((type) => Int, { nullable: true })
+  extra?: number;
+}
+
 @InputType('DishOptionInputType', { isAbstract: true })
 @ObjectType()
 class DishOption {
   @Field((type) => String)
   name: string;
 
-  @Field((type) => [String], { nullable: true })
-  choices: String[];
+  @Field((type) => [DishChoice], { nullable: true })
+  choices: DishChoice[];
 
   @Field((type) => Int, { nullable: true })
   extra?: number;
